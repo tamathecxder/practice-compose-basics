4cb5a27b41cdbc651b1a8d7d8b31e354c27b56ea
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-11-08 16:07:14 +0700

    implement ComposeArticle practice

--- app/src/main/java/com/tama/practicecomposebasics/ComposeArticle.kt
+++ app/src/main/java/com/tama/practicecomposebasics/ComposeArticle.kt
@@ -4,13 +4,27 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tama.practicecomposebasics.ui.theme.PracticeComposeBasicsTheme
 
 class ComposeArticle : ComponentActivity() {
@@ -19,11 +33,11 @@ class ComposeArticle : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeComposeBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArticleContainer(modifier = Modifier)
                 }
             }
         }
@@ -31,10 +45,39 @@ class ComposeArticle : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun ArticleContainer(modifier: Modifier = Modifier) {
+    val textModifier = modifier.padding(top = 16.dp)
+
+    Column {
+        BannerImage()
+        Column(modifier = modifier.padding(16.dp)) {
+            Text(
+                text = stringResource(R.string.header_text),
+                fontSize = 24.sp,
+                fontWeight = FontWeight.Medium,
+            )
+            Text(
+                text = stringResource(R.string.first_paragraph_text),
+                textModifier,
+                textAlign = TextAlign.Justify
+            )
+            Text(
+                text = stringResource(R.string.second_paragraph_text),
+                textModifier,
+                textAlign = TextAlign.Justify
+            )
+        }
+    }
+}
+
+@Composable
+fun BannerImage() {
+    val image = painterResource(R.drawable.bg_compose_background)
+
+    Image (
+        painter = image,
+        contentDescription = null,
+        contentScale = ContentScale.Fit
     )
 }
 
@@ -42,6 +85,6 @@ fun Greeting2(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview2() {
     PracticeComposeBasicsTheme {
-        Greeting2("Android")
+        ArticleContainer()
     }
 }
