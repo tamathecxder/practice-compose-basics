9ee90434190326d178a0b71c6bb2221df31bfd05
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-11-08 23:11:56 +0700

    init BusinessCardActivity

---
+++ app/src/main/java/com/tama/practicecomposebasics/BusinessCardActivity.kt
@@ -0,0 +1,47 @@
+package com.tama.practicecomposebasics
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.tama.practicecomposebasics.ui.theme.PracticeComposeBasicsTheme
+
+class BusinessCardActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            PracticeComposeBasicsTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting2(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting2(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview5() {
+    PracticeComposeBasicsTheme {
+        Greeting2("Android")
+    }
+}
