6d7482d289d6f9ecf4c7d7e484cebd183c496f0e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2024-11-08 16:23:39 +0700

    implement TaskManagerActivity

--- app/src/main/java/com/tama/practicecomposebasics/TaskManagerActivity.kt
+++ app/src/main/java/com/tama/practicecomposebasics/TaskManagerActivity.kt
@@ -4,13 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.tama.practicecomposebasics.ui.theme.PracticeComposeBasicsTheme
 
 class TaskManagerActivity : ComponentActivity() {
@@ -19,11 +30,11 @@ class TaskManagerActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             PracticeComposeBasicsTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting2(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface (
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ArticleContainer(modifier = Modifier)
                 }
             }
         }
@@ -31,17 +42,44 @@ class TaskManagerActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting2(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
+fun IconImage() {
+    val icon = painterResource(R.drawable.ic_task_completed)
+
+    Image (
+        painter = icon,
+        contentDescription = null,
+        contentScale = ContentScale.Fit
     )
+}
+
+@Composable
+fun TaskWrapper(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier
+            .fillMaxSize()
+            .padding(24.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        IconImage()
+
+        Text(
+            text = stringResource(R.string.all_tasks_completed_text),
+            fontWeight = FontWeight.Bold,
+            modifier = modifier.padding(top = 24.dp, bottom = 8.dp)
+        )
+
+        Text(
+            text = stringResource(R.string.compliment_text),
+            fontSize = 16.sp
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview3() {
     PracticeComposeBasicsTheme {
-        Greeting2("Android")
+        TaskWrapper(modifier = Modifier)
     }
 }
